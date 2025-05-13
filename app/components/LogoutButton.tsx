cf12779413ab9e7b6659c9f0d9022413208c4a28
Xander Beaulac <xbeaulac@gmail.com>
2025-05-13 12:33:34 -0700

    ui changes

--- app/components/LogoutButton.tsx
+++ app/components/LogoutButton.tsx
@@ -1,14 +1,12 @@
+import { Button } from "@/components/ui/button";
 import { logout } from "../actions/auth";
 
 export default function LogoutButton() {
   return (
     <form action={logout}>
-      <button
-        type="submit"
-        className="bg-red-500 text-white px-4 py-2 rounded-md"
-      >
+      <Button type="submit" variant="ghost">
         Logout
-      </button>
+      </Button>
     </form>
   );
 }
